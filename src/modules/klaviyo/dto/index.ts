8748755b4876efae3f08c8b9d2ffc428e6583aae
MD ISMAIL HOSEN <43248620+ismail61@users.noreply.github.com>
2023-11-01 18:50:36 +0600

    Update index.ts

--- src/modules/klaviyo/dto/index.ts
+++ src/modules/klaviyo/dto/index.ts
@@ -2,7 +2,9 @@ import { ApiProperty } from '@nestjs/swagger';
 import { IsString, IsNotEmpty } from 'class-validator';
 
 export class KlaviyoPrivateKeyQueryDto {
-  @ApiProperty()
+  @ApiProperty({
+    description: `Klaviyo uses public and private API keys to authenticate API requests. You can obtain and make changes to your API keys from your account's Settings page under the API Keys tab. To manage API keys, you must have an Owner, Admin, or Manager role on the account. See more: https://docs.getelevar.com/docs/how-to-create-klaviyo-private-api-key`,
+  })
   @IsString()
   @IsNotEmpty()
   privateApiKey: string;
